6fd0c5e6cd5c963632e8db42cdd7f37692ed0689
Alexey Zabashta <AlexZabashta@github.com>
2019-05-21 20:34:54 +0300

    add java example

--- example/ExtractMetaFeatures.java
+++ example/ExtractMetaFeatures.java
@@ -2,28 +2,47 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Random;
 import java.util.function.ToDoubleFunction;
-import java.util.stream.IntStream;
+import java.util.logging.LogManager;
 
-import ru.ifmo.ctddev.ml.mfe.Extractor;
+import ru.ifmo.ctddev.ml.mfe.Landmarks;
+import ru.ifmo.ctddev.ml.mfe.MetaFeatures;
+import ru.ifmo.ctddev.ml.mfe.Utils;
+import weka.core.Instances;
 
 public class ExtractMetaFeatures {
 
     public static void main(String[] args) {
-        int n = 128, m = 16;
-        double[][] dataset = generateDataset(n, m, new Random(42));
+        LogManager.getLogManager().reset();
+        final int objects = 128, features = 16, classes = 2;
 
-        int f = n / 2; // Number of objects with first class
-        int s = n - f; // Number of objects with second class
-        int[] classDistribution = { f, s };
+        double[][] data = generateDataset(objects, features, new Random(42));
 
-        String[] names = Extractor.list();
-        int[] metaFeatureIndexes = IntStream.range(0, 23).toArray();
-
-        double[] metaFeatures = Extractor.extract(n, m, dataset, classDistribution, metaFeatureIndexes);
-
-        for (int i = 0; i < metaFeatureIndexes.length; i++) {
-            System.out.println(names[metaFeatureIndexes[i]] + "  = " + metaFeatures[i]);
+        int[] labels = new int[objects];
+        for (int i = 0; i < objects; i++) {
+            if (i * 2 < objects) {
+                labels[i] = 0;
+            } else {
+                labels[i] = 1;
+            }
         }
+
+        long start = System.currentTimeMillis();
+        Instances instances = Utils.convert(objects, features, classes, data, labels);
+
+        double[] metaFeatures = MetaFeatures.extract(objects, features, classes, data, labels, instances);
+        for (int i = 0; i < metaFeatures.length; i++) {
+            System.out.println(MetaFeatures.name(i) + " = " + metaFeatures[i]);
+        }
+
+        double[] landmarks = Landmarks.extract(instances);
+
+        for (int i = 0; i < landmarks.length; i++) {
+            System.out.println(Landmarks.name(i) + " = " + landmarks[i]);
+        }
+        long finish = System.currentTimeMillis();
+
+        System.out.println(finish - start);
+
     }
 
     static double[][] generateDataset(int n, int m, Random random) {
